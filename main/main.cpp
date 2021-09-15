8aefeff27c00a2f0285afd06469fbfcfa7eee457
Barak Gonen <barakgonen9@gmail.com>
2021-09-15 07:41:00 -0700

    Making the mapping and drone work properly

--- main/main.cpp
+++ main/main.cpp
@@ -1,6 +1,7 @@
 #include<chrono>
 #include <iostream>
 #include <thread>
+#include <iostream>
 
 #include <opencv2/core/mat.hpp>
 #include <opencv2/core/types.hpp>
@@ -13,6 +14,7 @@
 #include "../drone_lib/include/ctello.h"
 
 #include "../orb_slam/include/System.h"
+#include "../orb_slam/include/Converter.h"
 
 using ctello::Tello;
 using cv::CAP_FFMPEG;
@@ -43,21 +45,36 @@ using g2o::VertexSim3Expmap;
 // URL where the Tello sends its video stream to.
 const char *const TELLO_STREAM_URL{"udp://0.0.0.0:11111?overrun_nonfatal=1&fifo_size=50000000"};
 
-int main(int argc, char **argv) {
+void sendACommand(Tello &tello, const std::string &command);
 
-   Tello tello{};
+void saveMap(ORB_SLAM2::System &SLAM) {
+    std::vector<ORB_SLAM2::MapPoint *> mapPoints = SLAM.GetMap()->GetAllMapPoints();
+    std::ofstream pointData;
+    pointData.open("/tmp/RoomCoordiantes.csv");
+    for (auto p: mapPoints) {
+        if (p != NULL) {
+            auto point = p->GetWorldPos();
+            Eigen::Matrix<double, 3, 1> v = ORB_SLAM2::Converter::toVector3d(point);
+            pointData << v.x() << "," << v.y() << "," << v.z() << std::endl;
+        }
+    }
+    pointData.close();
+}
+
+int main(int argc, char **argv) {
+    Tello tello{};
     if (!tello.Bind()) {
         return 0;
     }
 
     std::cout << "starting streaming on tello" << std::endl;
-    tello.SendCommand("streamon");
-    while (!(tello.ReceiveResponse())) { ; }
+    sendACommand(tello, "setfps high");
+    sendACommand(tello, "streamon");
+    sendACommand(tello, "speed 30");
 
     ORB_SLAM2::System slam("/local/RealTimeLearning/orb_slam/Vocabulary/ORBvoc.txt",
                            "/local/RealTimeLearning/orb_slam/config/TELLO.yaml",
-						   ORB_SLAM2::System::MONOCULAR, true);
-
+                           ORB_SLAM2::System::MONOCULAR, true);
 
     std::cout << "tello streaming started" << std::endl;
 
@@ -65,35 +82,92 @@ int main(int argc, char **argv) {
     VideoCapture capture{TELLO_STREAM_URL, CAP_FFMPEG};
     capture.set(CV_CAP_PROP_BUFFERSIZE, 5);
     double fps = capture.get(CV_CAP_PROP_FPS);
-
+    bool lostTracking = false;
     int frameCount = 0;
-    std::cout << "bla" << std::endl;
-    std::thread t([&tello](){
-        tello.SendCommand("takeoff");
-        while (!(tello.ReceiveResponse())) { ; }
-        tello.SendCommand("forward 30");
-        while (!(tello.ReceiveResponse())) { ; }
-        tello.SendCommand("cw 90");
-        tello.SendCommand("right 30");
-        while (!(tello.ReceiveResponse())) { ; }
-        while (!(tello.ReceiveResponse())) { ; }
-        tello.SendCommand("cw 90");
-        while (!(tello.ReceiveResponse())) { ; }
+    std::cout << fps << std::endl;
+
+    // Drone control thread
+    std::thread t([&tello, &lostTracking, &slam]() {
+        sendACommand(tello, "takeoff");
+        sendACommand(tello, "up 20");
+        lostTracking = false;
+        std::this_thread::sleep_for(std::chrono::milliseconds(3000));
+
+        int deg;
+        // Turn on
+        for (int i = 0; i < 12; ++i) {
+            if(lostTracking){
+                std::cout << "Lost tracking trying to localize" << std::endl;
+                sendACommand(tello, "cw 20");
+                std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+                sendACommand(tello, "back 35");
+                std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+                sendACommand(tello, "forward 35");
+                std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+                sendACommand(tello, "ccw 20");
+                std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+            }
+            // Localizing help
+            sendACommand(tello, "back 35");
+            std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+            sendACommand(tello, "forward 35");
+            std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+
+/*            // Move left
+            sendACommand(tello, "right 30");
+            std::this_thread::sleep_for(std::chrono::milliseconds(1500));
+
+            // Move right
+            sendACommand(tello, "left 30");
+            std::this_thread::sleep_for(std::chrono::milliseconds(1500));*/
+
+            // Turn left
+            sendACommand(tello, "ccw 30");
+
+            //Wait
+            std::this_thread::sleep_for(std::chrono::milliseconds(2500));
+        }
+
+        // take off
         tello.SendCommand("land");
-        while (!(tello.ReceiveResponse())) { ; }
     });
 
 
+    // Saving map thread
+    std::thread t2([&slam, &tello]() {
+        while (true) {
+            std::getchar();
+            std::cout << "EMERGENCYYYY" << std::endl;
+            tello.SendCommand("land");
+            tello.SendCommand("emergency");
+            saveMap(slam);
+        }
+    });
+
     while (true) {
-        cv::Mat colorMat;
+        cv::Mat greyMat, colorMat;
         capture.grab();
         capture >> colorMat;
-        if(!colorMat.empty()) {
-            slam.TrackMonocular(colorMat, frameCount / fps);
+        //cv::cvtColor(colorMat, greyMat, CV_BGR2GRAY);
+        if (!colorMat.empty()) {
+            cv::Mat slamMatrix = slam.TrackMonocular(colorMat, frameCount / 30);
+            if (slamMatrix.empty()) {
+                std::cout << "You are fuckeeeedddddddddddd" << std::endl;
+                lostTracking = true;
+            } else {
+                lostTracking = false;
+            }
             frameCount++;
         }
     }
 
-    std::cout << "DONE" << std::endl;
     return 0;
 }
+
+void sendACommand(Tello &tello, const std::string &command) {
+    tello.SendCommand(command);
+    std::cout << "sent command " << command << std::endl;
+    while (!(tello.ReceiveResponse())) { ; }
+    std::cout << "received response " << std::endl;
+}
+
